a10b775bc07ec91778acca6fce9e02fcc2ec5996
jaredmg18 <jaredgencarella18@gmail.com>
2017-01-19 11:38:34 -0800

    Update Hello.java

--- homework00/Hello.java
+++ homework00/Hello.java
@@ -1,6 +1,20 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*   File name        Greeter.java
+*   Purpose          Establish a greeter function
+*   Author           Jared Gencarella
+*   Date             1/17/17
+*   Notes            None
+*   Warnings         None
+*   Exceptions       None
+*
+*   Revision History
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*           Rev      Date    Modified by:   Reason for change/modification
+*          ----     ------  -------------  --------------------------------
+* @version 1.0.0
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 public class Greeter {
    public static void main ( String args []){
       System.out.println( "Hello world!");
 }
 }
-
