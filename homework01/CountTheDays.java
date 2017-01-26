095570443e02859ac548dbc7c7aa1ac58b9905ab
jaredmg18 <jaredgencarella18@gmail.com>
2017-01-26 12:00:01 -0800

    Create CountTheDays.java

---
+++ homework01/CountTheDays.java
@@ -0,0 +1,8 @@
+public class CountTheDays {
+  public static void main (String args[]) {
+    m1 = Long.parseLong)args[0];
+    System.out.println("m1 = " + m1);
+    CalendarStuff cs = new CalendarStuff();
+    cs.isLeapYear(year);
+  }
+}
