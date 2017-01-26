2e27201ecaa98f1703c3805fe7c1d9fcccf4bbf7
jaredmg18 <jaredgencarella18@gmail.com>
2017-01-26 12:43:11 -0800

    Update CalendarStuff.java

--- homework01/CalendarStuff.java
+++ homework01/CalendarStuff.java
@@ -249,3 +249,58 @@ public class CalendarStuff {
       }
     }
 
+    /**
+   * A method to return a count of the total number of days between two valid dates
+   * @param    month1 long   containing month number, starting with "1" for "January"
+   * @param    day1   long   containing day number
+   * @param    year1  long   containing four-digit year
+   * @param    month2 long   containing month number, starting with "1" for "January"
+   * @param    day2   long   containing day number
+   * @param    year2  long   containing four-digit year
+   * @return          long   count of total number of days
+   */
+   public static long daysBetween( long month1, long day1, long year1, long month2, long day2, long year2 ) {
+     long count = 0;
+      long year = year1;
+      long month = month1;
+      long day = day1;
+      if (dateEquals(month, day, year, month2, day2, year2)) {
+        return count;
+      } else  {
+        while (!dateEquals(month, day, year, month2, day2, year2)) {
+          count++;
+          day++;
+            if (day > daysInMonth(month, year)) {
+              day = 1;
+              month++;
+            }
+            if (month > DECEMBER) {
+            month = 0;
+            year++;
+          }
+          if (year > year2) {
+          year = year - 1;
+        }
+            // System.out.println("count" + count + "day" + day);
+            //  if ((daysInMonth(month, year) == day) && (month < month2)) {
+            //     month = month + 1;
+            //     day = 1;
+            //     day++;
+            //     count = count + 1;
+            //     System.out.println("count" + count + "day" + day + "month" + month);
+        //  }
+        //  if ((daysInMonth(month, year) == day) && (month == month2) && (year < year2)) {
+        //    year = year + 1;
+        //    month = 1;
+        //    day = 1;
+        //    day++;
+        //    count = count + 1;
+        //  }
+        //  if ((daysInMonth(month, year) == day) && (month == month2) && (year == year2)) {
+        //    return count;
+         }
+       }
+       return count;
+     }
+   }
+
