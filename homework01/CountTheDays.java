0f8abe80ac7fc1d1e80379a020dc118845f7ba4e
jaredmg18 <jaredgencarella18@gmail.com>
2017-01-28 00:39:30 -0800

    Update CountTheDays.java

--- homework01/CountTheDays.java
+++ homework01/CountTheDays.java
@@ -1,8 +1,23 @@
 public class CountTheDays {
-  public static void main (String args[]) {
-    m1 = Long.parseLong)args[0];
-    System.out.println("m1 = " + m1);
+  public static void main (String[] args) {
+    if (args.length == 6) {
     CalendarStuff cs = new CalendarStuff();
-    cs.isLeapYear(year);
-  }
+    long dayCount = cs.daysBetween(3,2,2001,4,2,2002);
+    //   month1 = Long.parseLong(args[0]);
+    // System.out.println("month1 = " + month1);
+    // month2 = Long.parseLong(args[1]);
+    // System.out.println("month2 = " + month2);
+    // day1 = Long.parseLong(args[1]);
+    // System.out.println("day1 = " + day1);
+    // day2 = Long.parseLong(args[1]);
+    // System.out.println("day2 = " + day2);
+    // year1 = Long.parseLong(args[1]);
+    // System.out.printl("year1 = " + year1);
+    // year2 = Long.parseLong(args[1]);
+    // System.out.println("year2 = " + year2);
+} else {
+  System.out.println("Not enough args");
+  System.exit(1);
+}
+}
 }
