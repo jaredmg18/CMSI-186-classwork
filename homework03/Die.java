657b3c8d898d131cf3a308ef0e65f41b5f000959
jaredmg18 <jaredgencarella18@gmail.com>
2017-02-16 11:53:52 -0800

    Update Die.java

--- homework03/Die.java
+++ homework03/Die.java
@@ -1,22 +1,37 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  File name     :  Dice-Games.java
- *  Purpose       :  A file full of stuff to do with the Java String class
- *  Author        :  B.J. Johnson
- *  Date          :  2017-02-09
- *  Student       :  Jared Gencarella
- *  Date          :  2017-02-14
- *  Description   :  This file presents a bunch of Dice-style helper methods. These methods can be used to play
-                     the game Yahtzee through the command line of a computer's terminal.
- *  Notes         :  None
+ *  File name     :  Die.java
+ *  Purpose       :  Provides a class describing a single die that can be rolled
+ *  @author       :  Jared Gencarella
+ *  Date          :  2017-02-06
+ *  Description   :  This class provides the data fields and methods to describe a single game die.  A
+ *                   die can have "N" sides.  Sides are randomly assigned sequential pip values, from 1
+ *                   to N, with no repeating numbers.  A "normal" die would thus has six sides, with the
+ *                   pip values [spots] ranging in value from one to six.  Includes the following:
+ *                   public Die( int nSides );                  // Constructor for a single die with "N" sides
+ *                   public int roll();                         // Roll the die and return the result
+ *                   public int getSides()                      // get the number of sides on this die     
+ *                   public int getValue()                      // get the value of this die
+ *                   public void setSides()                     // change the configuration and return the new number of sides
+ *                   public String toString()                   // Instance method that returns a String representation
+ *                   public static String toString()            // Class-wide method that returns a String representation
+ *                   public static void main( String args[] );  // main for testing porpoises
+ *
+ *  Notes         :  Restrictions: no such thing as a "two-sided die" which would be a coin, actually.
+ *                   Also, no such thing as a "three-sided die" which is a physical impossibility without
+ *                   having it be a hollow triangular prism shape, presenting an argument as to whether
+ *                   the inner faces are faces which then should be numbered.  Just start at four for
+ *                   minimum number of faces.  However, be aware that a four-sided die dosn't have a top
+ *                   face to provide a value, since it's a tetrahedron [pyramid] so you'll have to figure
+ *                   out a way to get the value, since it won't end up on its point.
+ *
  *  Warnings      :  None
- *  Exceptions    :  None
+ *  Exceptions    :  IllegalArgumentException when the number of sides or pips is out of range
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  Revision History
- *  ----------------
+ *  Revision Histor
+ *  ---------------
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
- *  @version 1.0.0  2017-01-19  B.J. Johnson  Initial writing and release
- *  @version 1.1.0  2017-01-22  B.J. Johnson  Fill in methods to make the program actually work
+ *  @version 1.0.0  2017-02-06  Jared Gencarella  Initial writing and release
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
