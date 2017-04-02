36f8abaf741ef1be90daac3454a6d3ac1b172936
jaredmg18 <jaredgencarella18@gmail.com>
2017-04-02 14:24:37 -0700

    Update Ball.java

--- homework05/Ball.java
+++ homework05/Ball.java
@@ -5,82 +5,114 @@ public class Ball {
   private static final double bRadius = 4.45; // radius of each ball in inches
   private static final double bWeight = 1; // weight of each ball in pounds
   private static final double timeSlice = 1; //default timeSlice in seconds
-  private double xpos = 0.0;
-  private double ypos = 0.0;
-  private double deltax = 0.0;
-  private double deltay = 0.0;
+  private double xpos;
+  private double ypos;
+  private double deltax;
+  private double deltay;
   private double totalSeconds = 0;
   private double angle = 0;
   private double Max_Speed = 131; // this is in miles per hour!!
+  private Timer t;
+  private double[] bPosition = new double[2];
+  private double[] deltaV = new double[2];
 
-  public Ball(double x, double y, double deltax, double deltay) {
+  public Ball(double x, double y, double dx, double dy) {
     xpos = x;
     ypos = y;
-    deltax = deltax;
-    deltay = deltay;
+    deltax = dx;
+    deltay = dy;
   }
 
   public void validatePosition(double xpos, double ypos) {
-    if (xpos < 300 && xpos > -300) {
+    if (xpos < 200 && xpos > -200) {
       xpos = xpos;
     } else {
-      throw new IllegalArgumentException("both starting coordinates for the ball must be between 300 and -300");
+      throw new IllegalArgumentException("both starting coordinates for the ball must be between 200 and -200");
     }
-    if (ypos < 300 && ypos > -300) {
+    if (ypos < 200 && ypos > -200) {
       ypos = ypos;
     } else {
-      throw new IllegalArgumentException("both starting coordinates for the ball must be between 300 and -300");
+      throw new IllegalArgumentException("both starting coordinates for the ball must be between 200 and -200");
     }
   }
 
-  public double updateXPosition() {
-    xpos += deltax;
-    deltax *= .99;
-    return xpos;
-      // double velocity = Math.sqrt((dx * dx) + (dy * dy));
-      // double nv = velocity * Math.pow(.99, this.totalSeconds);
+  public double[] updateSpeed() {
+    deltaV[0] = deltax;
+    deltaV[1] = deltay;
+    deltax = .99 * deltax;
+    deltay = .99 * deltay;
+    return deltaV;
     }
 
-    public double updateYPosition() {
-      ypos += deltay;
-      deltay *= .99;
-      return ypos;
+    public double[] updatePos() {
+      bPosition[0] = xpos + deltaV[0];
+      bPosition[1] = ypos + deltaV[1];
+      return bPosition;
     }
 
-    public double getXPos() {
-      return this.xpos;
+    public String posToString() {
+      return "[" + bPosition[0] + "," + bPosition[1] + "]";
     }
 
-    public double getYPos() {
-      return this.ypos;
-    }
-
-    public String toString() {
-      return "[" + xpos + "," + ypos + "]";
+    public String speedToString() {
+      return "[" + deltaV[0] + "," + deltaV[1] + "]";
     }
 
     public boolean isMoving() {
-      if((xpos < -300) || (xpos > 300)) {
+      if((xpos < -200) || (xpos > 200)) {
         return false;
       }
-      if ((ypos < -300) || (ypos > 300)) {
+      if ((ypos < -200) || (ypos > 200)) {
         return false;
       }
-      if((deltax <= .084) && (deltay <= .084)) {
+      if((deltax <= (1/12)) && (deltay <= (1/12))) {
         return false;
       }
       return true;
     }
 
   public static void main (String args[] ) {
-    // Position p1 = new Position (0.0, 0.0);
-    // Position p2 = new Position (1.0, 1.0);
-    // Position p3 = new Position (2.0, -3.0);
-    // System.out.println( "Testing Position creation: ");
-    // System.out.println( "Position p1 is: " + p1.toString());
-    // System.out.println( "Position p2 is: " + p2.toString());
-    // System.out.println( "Position p3 is: " + p3.toString());
+    Timer timer1 = new Timer(1);
+    System.out.println("Testing position with x value of 10 and y value of 30");
+    System.out.println("Testing velocity with x speed of 2 and y speed of 3");
+    System.out.println("Testing with a time increment of 1 second");
+    Ball b = new Ball(10, 30, 2, 3);
+    System.out.println("Time: " + timer1.toString());
+    b.updatePos();
+    System.out.println("Position: " + b.posToString());
+    b.updateSpeed();
+    System.out.println("Speed: " + b.speedToString());
 
-    System.out.println( "Testing position with x value of 10 and y value of 30");
+   timer1.tick();
+   System.out.println("Time: " + timer1.toString());
+   b.updatePos();
+   System.out.println("Position: " + b.posToString());
+   b.updateSpeed();
+   System.out.println("Speed: " + b.speedToString());
+
+   timer1.tick();
+   System.out.println("Time: " + timer1.toString());
+   b.updatePos();
+   System.out.println("Position: " + b.posToString());
+   b.updateSpeed();
+   System.out.println("Speed: " + b.speedToString());
+
+   Timer timer2 = new Timer(10);
+   System.out.println("Testing position with x value of 10 and y value of 30");
+   System.out.println("Testing velocity with x speed of 2 and y speed of 3");
+   System.out.println("Testing with a time increment of 10 seconds");
+   Ball b2 = new Ball(10, 30, 2, 3);
+   System.out.println("Time: " + timer2.toString());
+   b.updatePos();
+   System.out.println("Position: " + b.posToString());
+   b.updateSpeed();
+   System.out.println("Speed: " + b.speedToString());
+
+   timer2.tick();
+   System.out.println("Time: " + timer2.toString());
+   b.updatePos();
+   System.out.println("Position: " + b.posToString());
+   b.updateSpeed();
+   System.out.println("Speed: " + b.speedToString());
   }
 }
